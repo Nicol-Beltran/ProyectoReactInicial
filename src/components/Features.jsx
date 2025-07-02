b71aa7f968bd45d15417f294c23988b504ca9746
Nicol-Beltran <beltrannicole384@gmail.com>
2025-07-01 21:20:18 -0500

    Primeros componentes de react

---
+++ src/components/Features.jsx
@@ -0,0 +1,23 @@
+import imagen1 from "../assets/imagen1.png"
+export default function Features(){
+    return(
+        <>
+        <section id="features" className="container row ">
+            <h2>Caracteristicas principales</h2>
+            <div class="card" style={{width: "18rem"}}>
+  <img src="https://media.istockphoto.com/id/1420676204/es/foto/retrato-de-un-tigre-real-de-bengala-alerta-y-mirando-a-la-c%C3%A1mara-animal-nacional-de-bangladesh.jpg?s=1024x1024&w=is&k=20&c=-Wgtez5zKlIoxEWsNxJpC_FehTdfGzoCua_TPW9cafw=" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+</div>
+
+ <div class="card" style={{width: "18rem"}}>
+  <img src={imagen1} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+</div>
+        </section>
+        </>
+    )
+}
