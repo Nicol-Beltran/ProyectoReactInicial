270fa96e2fb7f44708dfffdc902c52fc937b24f6
Nicol-Beltran <beltrannicole384@gmail.com>
2025-07-01 21:42:36 -0500

    Estilos e importaciones

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,9 @@
 import imagen1 from "../assets/imagen1.png"
+import styles from "../components/features.module.css"
 export default function Features(){
     return(
         <>
-        <section id="features" className="container row ">
+        <section id="features" className={styles.features}>
             <h2>Caracteristicas principales</h2>
             <div class="card" style={{width: "18rem"}}>
   <img src="https://media.istockphoto.com/id/1420676204/es/foto/retrato-de-un-tigre-real-de-bengala-alerta-y-mirando-a-la-c%C3%A1mara-animal-nacional-de-bangladesh.jpg?s=1024x1024&w=is&k=20&c=-Wgtez5zKlIoxEWsNxJpC_FehTdfGzoCua_TPW9cafw=" class="card-img-top" alt="..."/>
